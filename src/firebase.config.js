10679d6f2ff105c7fc0c30d72ae0416036141fcc
silvanarl <ramossilvana495@gmail.com>
2020-11-23 11:56:57 -0500

    config inicial
    conexion a firebase, eslint, prettier, sass

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBN8Zf75GX7c9yFP6zVzBOikKc0Kh7VXEU",
+    authDomain: "interbank-project.firebaseapp.com",
+    databaseURL: "https://interbank-project.firebaseio.com",
+    projectId: "interbank-project",
+    storageBucket: "interbank-project.appspot.com",
+    messagingSenderId: "834607468586",
+    appId: "1:834607468586:web:5efe190aadfae832083353"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export default firebase;
