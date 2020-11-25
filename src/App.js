016e581b37001d38f42c76213ab601439b6f15b0
juli <nani.jimenez@hotmail.com>
2020-11-25 11:09:34 -0500

    search

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from './views/Login';
 import Menu from './views/Menu';
+import Search from './components/general/Search';
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/login">
-          <Login />
+        <Route exact path="/">
+          <Login />,
+          <Search/>
         </Route>
         <Route exact path="/menu">
           <Menu />
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+//import 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBN8Zf75GX7c9yFP6zVzBOikKc0Kh7VXEU",
@@ -14,4 +15,6 @@ const firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
+//export const storage = firebase.storage();
+
 export default firebase;
