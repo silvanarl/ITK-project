84012e2cbf7ef9fd7e7c38658eeaf2fdea5a3cb6
silvanarl <ramossilvana495@gmail.com>
2020-11-25 20:57:02 -0500

    initiatives

--- src/components/InitiativesCard.jsx
+++ src/components/InitiativesCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Search from '../components/general/Search';
+import Search from './general/Search';
 
 const FormInitiative = () => {
     return (
         <div>
             Soy un componente!
             <Search />
+            
         </div>
     )
 };
