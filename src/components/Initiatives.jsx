674d68a840b9ff0a339f57c2f539300d982da39a
Lina Ureña <lina8maria8@hotmail.com>
2020-11-27 08:40:05 -0500

    'Notes'

--- src/components/Initiatives.jsx
+++ src/components/Initiatives.jsx
@@ -3,19 +3,6 @@
 // import InitiativesCard from './InitiativesCard';
 // import PropTypes from 'prop-types';
 
-<<<<<<< HEAD
-const Initiatives = ({allInitiatives}) => {
-    return (
-        <div>
-            <h2 className="title">Iniciativas</h2>
-            <p className="description">Busca iniciativas por palabras clave</p>
-            <Search />
-            <div className="break"></div>
-            <InitiativesCard allInitiatives={allInitiatives} />
-        </div>
-    )
-};
-=======
 // const Initiatives = ({allInitiatives}) => {
 //     return (
 //         <div>
@@ -25,7 +12,6 @@ const Initiatives = ({allInitiatives}) => {
 //         </div>
 //     )
 // };
->>>>>>> d7e63008b334a621c4aef0f1036c9e7afbfe462b
 
 // Initiatives.propTypes = {
 //     allInitiatives: PropTypes.arrayOf(PropTypes.object).isRequired,
