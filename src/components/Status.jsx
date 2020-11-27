0b01504e87f969173f4885e5a3a572e71707ffce
Lina Ureña <lina8maria8@hotmail.com>
2020-11-27 09:44:43 -0500

    State of workshop

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -41,16 +41,16 @@ export default function Status() {
     <div className="container-status" id="status">
       <div className="title">{init.name}</div>
       <StatusNav setView={setView} />
-
-      <div>
-        <label htmlFor="">Puedes modificar el estado de esta iniciativa: </label>
-            <select onChange={(e)=> updateStatus(e)} name="status" id="">
+      <h2 className="form-title">Información de la iniciativa</h2>
+      <div className="item">
+      <span className="info-id-data">Esta iniciativa actualmente se encuentra en {init.status}</span>
+        <label className="label-mod" htmlFor="">Puedes modificar el estado de esta iniciativa</label>
+            <select className="large-select"onChange={(e)=> updateStatus(e)} name="status" id="">
                 <option value="Convocatoria a workshop">Convocatoria a Workshop</option>
                 <option value="Workshop">Workshop</option>
                 <option value="Resumen workshop">Resumen workshop</option>
                 <option value="Comite ERCP">Comité ERCP</option>
             </select>
-        <span>Esta iniciativa actualmente se encuentra en: {init.status}</span>
       </div>
       {view === 'convocatoria' ? (
         <Convocatoria obj={init} initID={initID} />
